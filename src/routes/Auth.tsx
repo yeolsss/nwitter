ff8bc490f4ce5524b526a8bbc4bae70bb0461747
yeol <yeol8810@gmail.com>
2022-11-03 10:40:30 +0900

    creating account email로 회원 가입 추가

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,5 +1,52 @@
+import { authService } from '@/fbConfig';
+import { FormEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+interface IForm {
+  email: string;
+  password: string;
+}
+
 function Auth() {
-  return <div>Auth</div>;
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const [newAccount, setNewAccount] = useState<boolean>(true);
+  const onValid = async ({ email, password }: IForm) => {
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password,
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('email', { required: true })}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          {...register('password', { required: true })}
+          type="password"
+          placeholder="Password"
+        />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
