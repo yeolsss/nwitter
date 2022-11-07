b0b8919c2df9cdc69f9dbbeca5c1ac016c970bf1
yeol <yeol8810@gmail.com>
2022-11-07 15:15:58 +0900

    3. form and database setup

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,28 @@
+import { useForm } from 'react-hook-form';
+
+interface IForm {
+  chat: string;
+}
+
 function Home() {
-  return <div>Home</div>;
+  const { register, handleSubmit, getValues } = useForm();
+  const onValid = ({ chat }: IForm) => {
+    console.log(chat);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('chat', { required: true })}
+          type="text"
+          placeholder="What'on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
